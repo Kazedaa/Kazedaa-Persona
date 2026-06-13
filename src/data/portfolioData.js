ef0367367b3a6faf5da9ca6fc18d69b27a14eec5
Kazedaa <hemangjamadagni@gmail.com>
2026-06-13 22:49:15 +0530

    Add content

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,12 +1,12 @@
 export const personalInfo = {
   name: "Hemang J Jamadagni",
   education: "MSCS @ University California of San Diego(UCSD)",
-  about: "I am a passionate developer with a deep interest in software engineering and game design. I love creating immersive web experiences, exploring new technologies, and contributing to open-source projects. Constantly learning and building.",
+  about: "I am an Applied AI and Computer Vision Researcher with a rigorous background in Systems Engineering. I am also an avid open source enthusiasts, open for a chat or collaborate on interesting projects.",
   socials: {
-    email: "mailto:your.email@example.com",
-    linkedin: "https://www.linkedin.com/in/hemangjamadagni/",
+    email: "mailto:hemangjamadagni@gmail.com",
+    linkedin: "https://www.linkedin.com/in/hemangjamadagni",
     github: "https://github.com/Kazedaa",
-    googleScholar: "https://scholar.google.com/"
+    googleScholar: "https://scholar.google.com/citations?user=3W8dWKMAAAAJ&hl=en"
   },
   educationList: [
     {
@@ -28,35 +28,35 @@ export const personalInfo = {
 export const highlights = [
   {
     id: 1,
-    image: "https://placehold.co/100x100/1a1a1a/ffffff?text=HL1",
+    image: "src/assets/MANTA_poster.png",
     description: "Authored MANTA: Physics-Informed Underwater Object Tracking, accepted to WACV 2026.",
     date: "March 2026",
-    link: "https://openaccess.thecvf.com/content/WACV2026/html/Srinath_MANTA_Physics-Informed_Generalized_Underwater_Object_Tracking_WACV_2026_paper.html"
+    link: "https://wacv.thecvf.com/virtual/2026/poster/373"
   },
   {
     id: 2,
-    image: "https://placehold.co/100x100/1a1a1a/ffffff?text=HL2",
+    image: "src/assets/ietf.png",
     description: "Presented a Congestion Control Evaluation Suite Design at the 124th IETF Meeting to the CCWG in Montreal, Canada.",
     date: "Nov 2025",
     link: "https://www.youtube.com/watch?v=elcRv2ruyDc"
   },
   {
     id: 3,
-    image: "https://placehold.co/100x100/1a1a1a/ffffff?text=HL2",
+    image: "src/assets/echo_news_85.png",
     description: "Developed eBAF, a kernel-level eBPF Ad Firewall that received a shout-out from eBPF Foundation Leader, Bill Mulligan in his newsletter Echo News.",
     date: "July 2025",
     link: "https://www.linkedin.com/pulse/echo-news-85-bill-mulligan-jcsgf/"
   },
   {
     id: 4,
-    image: "https://placehold.co/100x100/1a1a1a/ffffff?text=HL2",
+    image: "src/assets/undive_poster.jpg",
     description: "Co-Authored UnDIVE: Generalized Underwater Video Enhancement using Generative Priors, accepted to WACV 2025, and presented at Tucson, USA.",
     date: "July 2025",
     link: "https://openaccess.thecvf.com/content/WACV2025/html/Srinath_UnDIVE_Generalized_Underwater_Video_Enhancement_using_Generative_Priors_WACV_2025_paper.html"
   },
   {
     id: 5,
-    image: "https://placehold.co/100x100/1a1a1a/ffffff?text=HL2",
+    image: "src/assets/sswc.jpg",
     description: "Authored HARQ Soft Combining Using Bidirectional LSTMs, accepted and presented at SSWC 2024.",
     date: "July 2025",
     link: "https://link.springer.com/chapter/10.1007/978-981-96-1348-9_9"
@@ -140,114 +140,98 @@ export const projects = [
   {
     id: 1,
     title: "eBAF: eBPF Based Ad Firewall",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+1",
+    image: "src/assets/ebaf.png",
     description: "eBAF is a high-performance, kernel-level network adblocker utilizing eBPF/XDP. Engineered to be a robust packet filtering system that bypasses the standard Linux networking stack to inspect and drop ad-serving traffic in real-time with near-zero latency. It handles the shifting ad domains by integrating a dynamic DNS resolution mechanism to continuously update IP blacklists, and includes an interactive web dashboard for live traffic monitoring. This project allowed me to explore low-level systems architecture and network protocols, ultimately earning a shout-out from eBPF Foundation Leader, Bill Mulligan in his newsletter Echo News.",
     date: "Apr 2025",
-    link: ""
+    link: "https://github.com/Kazedaa/eBAF"
   },
   {
     id: 2,
     title: "Bad Apple but it's a Bootloader",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+2",
+    image: "src/assets/badapple_uefi.png",
     description: "A standalone UEFI application written in C that effectively acts as a bare-metal micro-operating system just to play a video during the computer's boot sequence. Bypassing the OS entirely, it interfaces directly with the motherboard's firmware and uses the UEFI Graphics Output Protocol (GOP) to stream video straight to the screen's framebuffer. To get around standard FAT32 file size limits, I wrote a custom video codec to compress the video into a 1-bit Run-Length Encoded (RLE) format that the C engine decodes on the fly.",
     date: "May 2026",
-    link: ""
+    link: "https://github.com/Kazedaa/Bad-Apple-but-its-a-bootloader"
   },
   {
     id: 3,
     title: "MANTA: Physics-Informed Generalized Underwater Object Tracking",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+3",
+    image: "src/assets/manta_showcase.gif",
     description: "MANTA is a state-of-the-art, physics-informed machine learning framework designed to solve severe aquatic visual distortions in underwater object tracking. Implements a novel dual-positive contrastive learning pipeline integrating Beer-Lambert optical priors, and a multi-stage tracking architecture that leverages geometric consistency to resolve complex occlusion and drift challenges. The framework achieves state-of-the-art performance across four major benchmarks (WebUOT-1M, UOT32, UTB180, UWCOT220), improving Success AUC by 6% while ensuring real-time efficiency, and culminated in a co-first author publication accepted at WACV 2026.",
     date: "May 2025",
-    link: ""
+    link: "https://github.com/Kazedaa/MANTA"
   },
   {
     id: 4,
     title: "Bad Apple on a Terminal",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+4",
+    image: "src/assets/badapple_terminal.png",
     description: "A silly project, rendering the Bad Apple music video on an ASCII Terminal.",
     date: "Jun 2025",
-    link: ""
+    link: "https://github.com/Kazedaa/badapple"
   },
   {
     id: 5,
     title: "ClariGPT",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+5",
+    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=ClariGPT",
     description: "ClariGPT is a full-stack, AI-powered research assistant chatbot leveraging a ReACT agent framework, Flask, and the Groq API to streamline the querying and summarization of arXiv academic papers. It involves a modular, multi-tool backend integrating PyMuPDF for robust PDF extraction and Pydantic for structured AI outputs, successfully bridging the gap between raw LLM reasoning capabilities and a seamless, interactive conversational interface for end users.",
     date: "Mar 2025",
-    link: ""
+    link: "https://github.com/Kazedaa/ClariGPT"
   },
   {
     id: 6,
     title: "HueShift: Breathing Life into Every Frame",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+6",
+    image: "src/assets/hueshift.gif",
     description: "HueShift is an end-to-end video colorization pipeline leveraging diffusion models, PyTorch, and OpenCV to automatically colorize grayscale video sequences. Implements the core machine learning infrastructure to perform frame-wise colorization, encompassing data preprocessing, model training and inference, and multi-resolution post-processing utilities. It tackles complex visual artifacts by implementing temporal consistency optimizations, keyframe detection, and deflickering techniques. This project culminated in a public demonstration at the NITK Executive Project Expo.",
     date: "Jun 2024",
-    link: ""
+    link: "https://github.com/Kazedaa/Hueshift-Video-Coloring"
   },
   {
     id: 7,
     title: "Clash-of-Legends: A Two Player Street Fighter Game",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+7",
+    image: "src/assets/col.png",
     description: "Clash of Legends is an interactive, browser-based 2D fighting game utilizing object-oriented JavaScript and HTML5 Canvas. It implements a robust core game loop to manage real-time player inputs, sprite rendering, and precise hitbox collision detection. It includes complex state management systems to handle dynamic combat mechanics, including real-time health tracking, round-based countdown timers, and a strict win/loss condition logic, delivering a responsive, arcade-style game.",
     date: "Sep 2024",
-    link: ""
+    link: "https://github.com/Kazedaa/Clash-of-Legends"
   },
   {
     id: 8,
     title: "UnDIVE: Generalized Underwater Video Enhancement Using Generative Priors",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+8",
+    image: "src/assets/undive_showcase.png",
     description: "UnDIVE is a generalized machine learning framework leveraging generative priors to solve severe aquatic visual distortions in underwater video media. Tackles the critical lack of ground-truth data and the inherent temporal inconsistencies of traditional single-image enhancement methods by designing a robust pipeline that ensures stable, high-fidelity frame sequences across diverse underwater environments. This research successfully bridged theoretical generative models with practical video enhancement challenges, culminating in a co-authored publication accepted at WACV 2025.",
     date: "Apr 2024",
-    link: ""
+    link: "https://github.com/suhas-srinath/undive"
   },
   {
     id: 9,
     title: "Daily DDPM Anime",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+9",
+    image: "src/assets/daily_anime_ddpm.png",
     description: "An Instagram AI bot that generates and posts anime pictures. The bot utilizes a deep learning model, specifically a Denoising Diffusion Probabilistic Model (DDPM), implemented with PyTorch. The generated images are then automatically posted to Instagram.",
     date: "May 2024",
-    link: ""
+    link: "https://github.com/Kazedaa/Daily-DDPM-Anime"
   },
   {
     id: 10,
     title: "A Hole New World: Seamless Image Inpainting",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+10",
+    image: "src/assets/a_whole_new_world.png",
     description: "Image inpainting is a computer vision task that involves filling in missing or damaged parts of an image. It has various applications including photo editing, image restoration, and object removal. The focus of this project is on image inpainting using PatchGAN, a type of Generative Adversarial Network (GAN), on the CelebA dataset, which contains over 200,000 celebrity images.",
     date: "Jan 2024",
-    link: ""
+    link: "https://github.com/Kazedaa/A-Whole-New-World-Seamless-Image-Inpainting"
   },
   {
     id: 11,
-    title: "Operating Systems Virtual Lab",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+11",
-    description: "The Virtual OS Lab Simulator is an educational tool designed to help users understand and visualize CPU Scheduling algorithms and InterProcess Communication (IPC) between web browser workers. It provides a graphical interface to experiment with various scheduling algorithms and observe how processes communicate with each other.",
-    date: "Jan 2024",
-    link: ""
+    title: "HARQ Soft Combining Using Bidirectional LSTMs",
+    image: "src/assets/harq.png",
+    description: "A deep learning-based error correction framework for wireless communications by optimizing the Hybrid Automatic Repeat Request (HARQ) protocol. A Bidirectional Long Short-Term Memory (Bi-LSTM) Autoencoder processes multiple corrupted transmission frames to seamlessly reconstruct a single, high-fidelity output frame. This framework successfully bridges sequence modeling with low-level network reliability, culminating in a co-first author publication in a Scopus-indexed Springer book chapter and a personal presentation at the SSWC2024 International Conference.",
+    date: "Jul 2023",
+    link: "https://github.com/Kazedaa/HARQ-Soft-Combining-Using-Bidirectional-LSTMs"
   },
   {
     id: 12,
-    title: "HARQ Soft Combining Using Bidirectional LSTMs",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+12",
-    description: "A deep learning-based error correction framework for wireless communications by optimizing the Hybrid Automatic Repeat Request (HARQ) protocol. A Bidirectional Long Short-Term Memory (Bi-LSTM) Autoencoder processes multiple corrupted transmission frames to seamlessly reconstruct a single, high-fidelity output frame. This framework successfully bridges sequence modeling with low-level network reliability, culminating in a co-first author publication in a Scopus-indexed Springer book chapter and a personal presentation at the SSWC2024 International Conference.",
-    date: "Jul 2023",
-    link: ""
-  },
-  {
-    id: 13,
     title: "End-to-End Encrypted Communication Using Purely Digital Systems",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+13",
+    image: "src/assets/e2e_encrypted_comm.png",
     description: "The goal of this project is to design a system of units that communicate between each other via end-to-end encryption. The RSA encryption algorithm will be used and the user will be able to choose the kind of encryption. The aim is to achieve secure communication between two digital systems.",
     date: "Aug 2023",
-    link: ""
-  },
-  {
-    id: 14,
-    title: "CutiePatootie: A CatBot — Discord Bot for Posting Cat Pictures",
-    image: "https://placehold.co/400x250/1a1a1a/ffffff?text=Project+14",
-    description: "CatBot is a simple Discord bot built with Python that posts random cat pictures to your server. (My first ever project as a High School Student :D)",
-    date: "Oct 2022",
-    link: ""
+    link: "https://github.com/AdiPadi2703/End-To-End-Encrypted-Communication"
   }
 ];
 
@@ -256,27 +240,27 @@ export const publications = [
     id: 1,
     title: "MANTA: Physics-Informed Generalized Underwater Object Tracking",
     publisher: "Proceedings of the IEEE/CVF Winter Conference on Applications of Computer Vision (WACV), 2026, pp. 3472-3482",
-    image: "https://placehold.co/400x250/222222/ffffff?text=Pub+1",
+    image: "src/assets/manta_overview.png",
     description: "Underwater object tracking is challenging due to wavelength-dependent attenuation and scattering, which severely distort appearance across depths and water conditions. Existing trackers trained on terrestrial data fail to generalize to these physics-driven degradations. We present MANTA, a physics-informed framework integrating representation learning with tracking design for underwater scenarios. We propose a dual-positive contrastive learning strategy coupling temporal consistency with Beer–Lambert augmentations to yield features robust to both temporal and underwater distortions. We further introduce a multi-stage pipeline augmenting motion-based tracking with a physics-informed secondary association algorithm that integrates geometric consistency and appearance similarity for re-identification under occlusion and drift. To complement standard IoU metrics, we propose Center–Scale Consistency (CSC) and Geometric Alignment Score (GAS) to assess geometric fidelity. Experiments on four underwater benchmarks (WebUOT-1M, UOT32, UTB180, UWCOT220) show that MANTA achieves state-of-the-art performance, improving Success AUC by up to 6%, while ensuring stable long-term generalized underwater tracking and efficient runtime.",
     date: "Mar 6, 2026",
-    link: "https://scholar.google.com/"
+    link: "https://openaccess.thecvf.com/content/WACV2026/html/Srinath_MANTA_Physics-Informed_Generalized_Underwater_Object_Tracking_WACV_2026_paper.html"
   },
   {
     id: 2,
     title: "HARQ Soft Combining Using Bidirectional LSTMs",
     publisher: "Smart Innovation, Systems and Technologies ((SIST,volume 433)), p 109-122",
-    image: "https://placehold.co/400x250/222222/ffffff?text=Pub+1",
+    image: "src/assets/harq.png",
     description: "Hybrid Automatic Repeat reQuest is used in modern wireless data communication to integrate both Automatic Repeat reQuest and high-rate Forward Error Correction mechanisms to enhance the reliability of data transmission. Unlike traditional ARQ, where an error-ridden frame is discarded upon reception, HARQ temporarily stores the erroneous frame in a buffer. When the re-transmission of the same frame occurs, these two frames are combined to generate a new frame, trying to minimize errors. This is HARQ with Soft Combining. Existing methods like Chase Combining (Type-I) and Incremental Redundancy (Type-II and Type-III) implement Log-Likelihood Ratio and Maximum Ratio Combining to combine two erroneous frames. This paper uses a Bidirectional Long Short-Term Memory model to combine two frames with high channel noise errors. This paper introduces the BiLSTM model, which aims to reduce the Bit Error Rate and provides an approach for integrating this model into the existing HARQ structure.",
     date: "Apr 18, 2025",
-    link: "https://scholar.google.com/"
+    link: "https://link.springer.com/chapter/10.1007/978-981-96-1348-9_9"
   },
   {
     id: 3,
     title: "UnDIVE: Generalized Underwater Video Enhancement using Generative Priors",
     publisher: "Proceedings of the Winter Conference on Applications of Computer Vision (WACV), 2025, pp. 8983-8994",
-    image: "https://placehold.co/400x250/222222/ffffff?text=Pub+1",
+    image: "src/assets/undive_overview.png",
     description: "With the rise of marine exploration, underwater imaging has gained significant attention as a research topic. Underwater video enhancement has become crucial for real-time computer vision tasks in marine exploration. However, most existing methods focus on enhancing individual frames and neglect video temporal dynamics, leading to visually poor enhancements. Furthermore, the lack of ground-truth references limits the use of abundant available underwater video data in many applications. To address these issues, we propose a two-stage framework for enhancing underwater videos. The first stage uses a denoising diffusion probabilistic model to learn a generative prior from unlabeled data, capturing robust and descriptive feature representations. In the second stage, this prior is incorporated into a physics-based image formulation for spatial enhancement, while also enforcing temporal consistency between video frames. Our method enables real-time and computationally-efficient processing of high-resolution underwater videos at lower resolutions, and offers efficient enhancement in the presence of diverse water-types. Extensive experiments on four datasets show that our approach generalizes well and outperforms existing enhancement methods.",
     date: "Mar 3, 2025",
-    link: "https://scholar.google.com/"
+    link: "https://openaccess.thecvf.com/content/WACV2025/html/Srinath_UnDIVE_Generalized_Underwater_Video_Enhancement_using_Generative_Priors_WACV_2025_paper.html"
   },
 ];
